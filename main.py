6503899ca26e35df9448437236fccdfb253aaccf
Maxbeurer <haaandrew.feng@gmail.com>
2025-11-10 13:06:03 +0100

    react search agent original

---
+++ main.py
@@ -0,0 +1,33 @@
+from calendar import c
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="deepseek-chat")
+react_prompt = hub.pull("hwchase17/react")
+agent=  create_react_agent(
+    llm=llm, 
+    tools=tools, 
+    prompt=react_prompt,
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+def main():
+    result = chain.invoke(
+        input = {
+            "input": "search for 3 job posting for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
+
+
+
+if __name__ == "__main__":
+    main()
